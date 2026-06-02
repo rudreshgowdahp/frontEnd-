cde3729ab7e095a79df5ab365955047178aa50e5
rudresh <rudreshgowdahp@gmail.com>
2026-06-02 15:38:05 +0530

    update

---
+++ html/newPage.js
@@ -0,0 +1,33 @@
+
+function fun( name){
+    console.log("Name is"+name);
+}
+fun("rudra");
+function fun( name,age){
+    console.log("Name is"+name);
+    console.log("Age is"+age);
+}
+fun("rudra",23);
+function fun( name,age,id){
+     console.log("Name is"+name);
+    console.log("Age is"+age);
+    console.log("Id is "+id);
+}
+fun("rudra",23,1);
+let fruits=["Apple","Mango","Orange","Banana","pineapple"];
+console.log(fruits[0]);
+console.log(fruits.length);
+for(i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+for(let fruit of fruits){
+    console.log(fruit);
+}
+fruits.push("Guava");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.unshift("guava");
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
